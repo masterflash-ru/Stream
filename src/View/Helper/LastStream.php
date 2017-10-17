88cff0bfda5bfc521422aedf28b92b0d8288d8f3
Masterflash <sxq@yandex.ru>
2017-10-17 18:29:04 +0300

    fix

--- src/View/Helper/LastStream.php
+++ src/View/Helper/LastStream.php
@@ -21,16 +21,30 @@ class LastStream extends AbstractHelper
 * собственног помощник, вызывается в view:
 * echo $this->laststream(...........);
 * возвращается последние статьи в виде HTML 
-* параметры:
+* 
 * $stream_name - строка имени потока, например, news
-* $item_count - кол-во элементов, по умолчанию 3
-* $locale - строка имени локали, по умолчанию ru_RU
+параметры-опции:
+* items - кол-во элементов, по умолчанию 3
+* locale - строка имени локали, по умолчанию ru_RU
 */
-public function __invoke($stream_name,$item_count=3,$locale="ru_RU")
+public function __invoke($stream_name,array $options=null)// $item_count=3,$locale="ru_RU")
 {
-	$this->StreamLib->SetStreamName($stream_name);
-	$this->StreamLib->SetLocale($locale);
-	$items=$this->StreamLib->LoadLastList($item_count);
+    if (isset($options["locale"])) {
+        $locale=$options["locale"];
+    }else {
+        $locale="ru_RU";
+    }
+   
+    if (isset($options["items"])) {
+        $items=(int)$options["items"];
+    }else {
+        $items=3;
+    }
+
+    
+    $this->StreamLib->setStreamName($stream_name);
+	$this->StreamLib->setLocale($locale);
+	$items=$this->StreamLib->loadLastList($items);
 
 	$view=$this->getView();
 	$vm=new ViewModel(["items"=>$items,'locale'=>$locale]);

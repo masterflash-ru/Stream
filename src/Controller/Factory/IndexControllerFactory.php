93a21fc70c98187995f3efa3aafe28e21c6cd6b6
Masterflash <sxq@yandex.ru>
2018-12-04 11:28:22 +0300

    изменение соединения ADO

--- src/Controller/Factory/IndexControllerFactory.php
+++ src/Controller/Factory/IndexControllerFactory.php
@@ -14,7 +14,7 @@ class IndexControllerFactory implements FactoryInterface
 {
     public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
     {
-        $connection=$container->get('ADO\Connection');
+        $connection=$container->get('DefaultSystemDb');
         $stream_service = $container->get(Stream::class);
 		return new $requestedName($connection,$stream_service);
     }

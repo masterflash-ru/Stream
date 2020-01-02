db7dbb6cc609b910a3e68deb10b5e4b4a7e12efe
Masterflash <sxq@yandex.ru>
2020-01-02 10:06:36 +0300

    n

--- src/Controller/Factory/IndexControllerFactory.php
+++ src/Controller/Factory/IndexControllerFactory.php
@@ -15,7 +15,7 @@ class IndexControllerFactory implements FactoryInterface
     public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
     {
         $config = $container->get('Config');
-        $connection=$container->get($config["streams"]["database"]);
+        $connection=$container->get($config["streams"]["config"]["database"]);
         $stream_service = $container->get(Stream::class);
 		return new $requestedName($connection,$stream_service);
     }
--- src/Service/Factory/StreamFactory.php
+++ src/Service/Factory/StreamFactory.php
@@ -13,8 +13,8 @@ class StreamFactory implements FactoryInterface
 public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
     {
 		 $config = $container->get('Config');
-		 $connection=$container->get($config["stream"]["database"]);
-		 $cache = $container->get($config["stream"]["cache"]);
+		 $connection=$container->get($config["streams"]["config"]["database"]);
+		 $cache = $container->get($config["streams"]["config"]["cache"]);
         
         return new $requestedName($connection, $cache,$config);
     }
